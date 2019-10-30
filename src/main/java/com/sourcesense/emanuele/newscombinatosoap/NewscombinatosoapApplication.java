17395c94370772314f24c720a3000dc53c40f3ff
Emanuele Picariello <emanuele.picariello@sourcesense.com>
2019-10-30 09:36:31 +0100

    primo commit

---
+++ src/main/java/com/sourcesense/emanuele/newscombinatosoap/NewscombinatosoapApplication.java
@@ -0,0 +1,13 @@
+package com.sourcesense.emanuele.newscombinatosoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscombinatosoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewscombinatosoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sourcesense/emanuele/newscombinatosoap/NewscombinatosoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcesense.emanuele.newscombinatosoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscombinatosoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
